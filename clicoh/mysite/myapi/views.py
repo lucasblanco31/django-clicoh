7bd0b55a25887bfa709d923ef4059aacd7c5457f
Lucas Blanco <lucasblanco3107@gmail.com>
2020-10-24 12:32:02 -0300

    Se modifica views

--- clicoh/mysite/myapi/views.py
+++ clicoh/mysite/myapi/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import (CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin)
 
 from .serializers import ProductSerializer, OrderSerializer, OrderDetailSerializer
 from .models import Product, Order, OrderDetail
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(GenericViewSet,    
+                    CreateModelMixin, 
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                    ListModelMixin):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderViewSet(GenericViewSet,    
+                    CreateModelMixin, 
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                    ListModelMixin):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-class OrderDetailViewSet(viewsets.ModelViewSet):
+class OrderDetailViewSet(GenericViewSet,    
+                    CreateModelMixin, 
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                    ListModelMixin):
     queryset = OrderDetail.objects.all()
     serializer_class = OrderDetailSerializer
 
